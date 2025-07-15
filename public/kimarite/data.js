2057b39021aaf433fad3cbb95a1dd6e63dd63afb
Chris Bowers <pcbowers@gmail.com>
2025-07-15 19:42:57 -0400

    feat: ✨ Add kimarite breakdown

---
+++ public/kimarite/data.js
@@ -0,0 +1,346 @@
+window.kimariteData = [
+  {
+    name: 'fumidashi',
+    description: `One of the five ways to lose a bout. Accidentally taking a step outside of the ring despite no attack being initiated by the opponent.`
+  },
+  {
+    name: 'tokkurinage',
+    description: `Holding the opponent's head or neck with both hands and twisting him down to the ground.`
+  },
+  {
+    name: 'tsukihiza',
+    description: `One of the five ways to lose a bout. Falling down to the knee(s) through one's own mistake, and not being forced down by the opponent.`
+  },
+  {
+    name: 'okuritsuriotoshi',
+    description: `Stepping behind the opponent and lifting him up by grabbing his mawashi before slamming him down to the ground.`
+  },
+  {
+    name: 'okurigake',
+    description: `Stepping behind an opponent and hooking a leg around one of the his legs and pulling it toward you to force the opponent down.`
+  },
+  {
+    name: 'koshikudake',
+    description: `Losing by accidently falling over backwards without being pushed.`
+  },
+  {
+    name: 'kozumatori',
+    description: `Grabbing an off-balanced opponent's leg and lifting it up to force him down.`
+  },
+  {
+    name: 'osakate',
+    description: `Reaching around an opponent to grab his mawashi from behind and pulling him down.`
+  },
+  {
+    name: 'isamiashi',
+    description: `Losing by inadvertently stepping outside the ring while attacking an opponent.`
+  },
+  {
+    name: 'kotehineri',
+    description: `Grabbing an opponent's arm and twisting him to the side of the arm to force him down.`
+  },
+  {
+    name: 'yobimodoshi',
+    description: `Pulling an opponent to throw him off balance and then slamming him down in the opposite direction.`
+  },
+  {
+    name: 'waridashi',
+    description: `Using one hand to push an opponent out of the ring while grabbing his mawashi with the other.`
+  },
+  {
+    name: 'kimetaoshi',
+    description: `Wrapping one's arms around the opponent's arms or shoulders and forcing him down to the ground or out of the ring.`
+  },
+  {
+    name: 'sabaori',
+    description: `Grabbing the opponent's mawashi to pull him forward and forcing him down to his knees.`
+  },
+  {
+    name: 'okurihikiotoshi',
+    description: `Stepping into the opponent's behind and pulling him down to the ground.`
+  },
+  {
+    name: 'gasshohineri',
+    description: `Interlacing one's fingers behind the opponent's head, neck or back and twisting him down.`
+  },
+  {
+    name: 'nimaigeri',
+    description: `While pulling the opponent up to make him off balance, kicking the outside of his planted leg to throw him down.`
+  },
+  {
+    name: 'harimanage',
+    description: `When the opponent rushes low, grabbing his foe's mawashi from the back and pulling him to force him down or out of the ring.`
+  },
+  {
+    name: 'ushiromotare',
+    description: `When the opponent steps behind the wrestler, leaning backward and pushing him down or out of the ring.`
+  },
+  {
+    name: 'tsukite',
+    description: `One of five non-techniques (way to lose). The opponent stumbles by himself and lands on one or both hands.`
+  },
+  {
+    name: 'tsukaminage',
+    description: `Grabbing the opponent's mawashi, then lifting up his body before throwing him backward.`
+  },
+  {
+    name: 'watashikomi',
+    description: `Grabbing the opponent's thigh or knee with one hand, then pushing him off balance to force him down.`
+  },
+  {
+    name: 'zubuneri',
+    description: `Pushing one's head onto the opponent's chest or shoulder, then grabbing his arm and twisting him down to the ground.`
+  },
+  {
+    name: 'kirikaeshi',
+    description: `Placing a leg behind the opponent's knee, then pulling him and throwing backward.`
+  },
+  {
+    name: 'okuritsuridashi',
+    description: `Lifting up the opponent by his mawashi from behind and forcing him out of the ring.`
+  },
+  {
+    name: 'tasukizori',
+    description: `After wrapping an arm around the opponent's arm and holding his leg with the other arm, forcing him down by twisting the body and leaning to him.`
+  },
+  {
+    name: 'sototasukizori',
+    description: `While holding the opponent's arm with an arm, lifting up his leg with the other arm to throw him backward.`
+  },
+  {
+    name: 'amiuchi',
+    description: `Grabbing the opponent's arm with both hands and pulling it to throw him backward.`
+  },
+  {
+    name: 'tsukitaoshi',
+    description: `Pushing the opponent hard and making him trip over the edge out of the ring onto his back.`
+  },
+  {
+    name: 'hikkake',
+    description: `Pulling the opponent's arm and tripping or forcing him out of the ring.`
+  },
+  {
+    name: 'tsutaezori',
+    description: `Pulling the opponent's arm around and twisting him onto his back.`
+  },
+  {
+    name: 'yaguranage',
+    description: `While grabbing the opponent's mawashi, lifting him up by using a leg and throwing him down on his side.`
+  },
+  {
+    name: 'chongake',
+    description: `Tripping the opponent by hooking his heel behind the opponent's heel and forcing him to fall over by twisting him.`
+  },
+  {
+    name: 'makiotoshi',
+    description: `Twisting the opponent's body down to the ground without grasping the mawashi.`
+  },
+  {
+    name: 'mitokorozeme',
+    description: `Wrapping a leg around the backside of the opponent's leg while grabbing his opposite thigh and pushing his chest to force him down backward.`
+  },
+  {
+    name: 'shumokuzori',
+    description: `Lifting the opponent on one's back and falling backward so that he falls to the ground first.`
+  },
+  {
+    name: 'tottari',
+    description: `Wrapping both arms around the opponent's arm and twisting him outside to force him down.`
+  },
+  {
+    name: 'sakatottari',
+    description: `Counter the opponent's tottari by pulling the arm out of the his grasp to make him off balance or use the body to force him out.`
+  },
+  {
+    name: 'kakenage',
+    description: `Hooking a leg around the opponent's leg and lifting it up to force him off balance.`
+  },
+  {
+    name: 'okurinage',
+    description: `Stepping behind the opponent and pushing him out of the ring.`
+  },
+  {
+    name: 'tsuriotoshi',
+    description: `Lifting up the opponent by holding his mawashi, then slamming him down to the ground.`
+  },
+  {
+    name: 'kimedashi',
+    description: `Restricting the opponent's elbows or shoulder joints by wrapping both arms around him and forcing him out of the ring.`
+  },
+  {
+    name: 'sotokomata',
+    description: `After unleashing a throw, grabbing the opponent's thigh from the outside and lifting it up to throw him down on his back.`
+  },
+  {
+    name: 'sokubiotoshi',
+    description: `Hitting the opponent's neck or back of head to force him down.`
+  },
+  {
+    name: 'koshinage',
+    description: `Getting under the opponent's body to lift him up over the hip and throw him down.`
+  },
+  {
+    name: 'omata',
+    description: `After unleashing a throw and forcing the opponent to stand on one foot, lifting the thigh of his other leg and forcing him backward and down.`
+  },
+  {
+    name: 'kainahineri',
+    description: `Grabbing the opponent's arm with both arms and twisting him forward and down.`
+  },
+  {
+    name: 'kubihineri',
+    description: `Putting an arm around the opponent's neck and twisting it down to force him down.`
+  },
+  {
+    name: 'kakezori',
+    description: `Putting one's head under the opponent's armpit while hooking a leg under the opponent's leg and forcing him down backwards.`
+  },
+  {
+    name: 'susotori',
+    description: `While the opponent is trying to unleash a throw, grabbing his ankle and pulling him down.`
+  },
+  {
+    name: 'okuritaoshi',
+    description: `Getting behind the opponent and pushing him down.`
+  },
+  {
+    name: 'hikiotoshi',
+    description: `Using the opponent's forward momentum and pulling on his shoulder, arm or mawashi to force him down.`
+  },
+  {
+    name: 'ketaguri',
+    description: `Right after the start of a fight, sweeping the opponent's inside ankle to throw him off balance and forcing him down to the ground.`
+  },
+  {
+    name: 'susoharai',
+    description: `After unleashing a throw, sweeping the opponent's ankle and pulling him down backward to the ground.`
+  },
+  {
+    name: 'komatasukui',
+    description: `After unleashing a throw, grabbing the inside thigh of the opponent's near leg and lifting it up,Sending him off balance.`
+  },
+  {
+    name: 'yoritaoshi',
+    description: `Holding the opponent's mawashi and forcing him backward to make him collapse out of the ring.`
+  },
+  {
+    name: 'sotomuso',
+    description: `Grabbing the knee of the opponent's inside leg and twisting him down to the ground.`
+  },
+  {
+    name: 'uchimuso',
+    description: `Grabbing the knee of the opponent's outside leg with the upper hand and twisting him down to the ground.`
+  },
+  {
+    name: 'sukuinage',
+    description: `Extending an arm under the opponent's underarm while stepping aside and throwing him down to the ground without grabbing his mawashi.`
+  },
+  {
+    name: 'oshidashi',
+    description: `Pushing the opponent on his chest, throat or underarms to force him out of the ring without holding his mawashi. One of the popular kimarite in sumo. If the opponent falls down, it is called oshitaoshi.`
+  },
+  {
+    name: 'tsukidashi',
+    description: `Pushing the opponent's face or chest to force him out of the ring with one or a series of palm thrusts.`
+  },
+  {
+    name: 'hatakikomi',
+    description: `Slapping down on the opponent's shoulder or back to force him down to the ground.`
+  },
+  {
+    name: 'uwatehineri',
+    description: `Grabbing the opponent's mawashi over his arm and twisting him down backward to the ground.`
+  },
+  {
+    name: 'shitatehineri',
+    description: `Grabbing the opponent's mawashi under his arm and pulling him down forward to the ground.`
+  },
+  {
+    name: 'kotenage',
+    description: `Wrapping an arm around the opponent's arm and throwing him to the ground without grabbing his mawashi.`
+  },
+  {
+    name: 'uwatedashinage',
+    description: `Grabbing the opponent's mawashi over his arm, then throwing him down.`
+  },
+  {
+    name: 'shitatedashinage',
+    description: `Grabbing the opponent's mawashi under his arm, then pulling him forward and down.`
+  },
+  {
+    name: 'ashitori',
+    description: `Grabbing the opponent's leg with both hands and leaning forward to force him to go off balance and fall down.`
+  },
+  {
+    name: 'shitatenage',
+    description: `Extending an arm under the opponent's arm to grab the opponent's mawashi, then throwing down the opponent while turning sideways.`
+  },
+  {
+    name: 'katasukashi',
+    description: `Grasping the opponent's shoulders with both hands and forcing him down by turning sideways and pushing down his shoulder with a hand.`
+  },
+  {
+    name: 'tsuridashi',
+    description: `Lifting the opponent by grabbing his mawashi and forcing him out of the dohyo.`
+  },
+  {
+    name: 'nichonagee',
+    description: `Hooking a leg around the outside of the opponent's knee and unleashing a throw by sweeping both of his legs.`
+  },
+  {
+    name: 'uchigake',
+    description: `Hooking the calf around the opponent's calf from inside and forcing him down.`
+  },
+  {
+    name: 'sotogake',
+    description: `Hooking the calf around the opponent's calf from outside and forcing him down.`
+  },
+  {
+    name: 'ipponzeoi',
+    description: `Grabbing the opponent's arm with both hands and pulling it down over the shoulder.`
+  },
+  {
+    name: 'kawazugake',
+    description: `Wrapping a leg around the opponent's opposite leg from the inside and pulling him backwards.`
+  },
+  {
+    name: 'abisetaoshi',
+    description: `Forcing the opponent down on his back by leaning on him.`
+  },
+  {
+    name: 'izori',
+    description: `Grabbing the opponent's legs and pulling upward with both arms, causing him to fall over.`
+  },
+  {
+    name: 'utchari',
+    description: `When forced to the edge of the dohyo, bending backward and twisting the opponent out of the ring.`
+  },
+  {
+    name: 'kubinage',
+    description: `Wrapping an arm around the opponent's neck and throwing him down to the ground.`
+  },
+  {
+    name: 'uwatenage',
+    description: `Throwing the opponent by grabbing the mawashi with an arm that is extended over his foe's arm.`
+  },
+  {
+    name: 'okuridashi',
+    description: `Turning to the opponent's side or rear and pushing him out of the ring.`
+  },
+  {
+    name: 'oshitaoshi',
+    description: `Striking the opponent's body palms-first to push him to the ground.`
+  },
+  {
+    name: 'tsukiotoshi',
+    description: `Pushing the opponent down to the ground by exerting force on his upper torso.`
+  },
+  {
+    name: 'kekaeshi',
+    description: `Kicking the inside of the opponent's ankle with the sole of the foot while pulling the opponent's shoulder to force them off balance.`
+  },
+  {
+    name: 'yorikiri',
+    description: `Pushing the opponent forward or laterally to force him out of the ring with grabbing the mawashi (belt). One of the most popular techniques.`
+  }
+];
